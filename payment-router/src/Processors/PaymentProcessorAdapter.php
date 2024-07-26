e9568fca37bad3fcd7b8fb8d2eefe856a6cbf515
Kelvin Chikezie <kevchike@gmail.com>
2024-07-26 16:29:32 +0100

    Add a payment processor adapter to handle multiple payment processors

---
+++ payment-router/src/Processors/PaymentProcessorAdapter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kevchikezie\PaymentRouter\Processors;
+
+use Kevchikezie\PaymentRouter\Contracts\PaymentProcessorInterface;
+
+abstract class PaymentProcessorAdapter implements PaymentProcessorInterface
+{
+    protected $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    abstract public function processPayment(array $paymentDetails): bool;
+    abstract public function getTransactionCost(): float;
+    abstract public function getReliabilityScore(): float;
+    abstract public function supportsCurrency(string $currency): bool;
+}

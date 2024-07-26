08bbb63c3394398d174a2209b16110ec05a74677
Kelvin Chikezie <kevchike@gmail.com>
2024-07-27 00:22:43 +0100

    Register and boot service provider

--- src/PaymentRouterServiceProvider.php
+++ src/PaymentRouterServiceProvider.php
@@ -8,11 +8,28 @@ class PaymentRouterServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/payment-router.php',
+            'payment-router'
+        );
+
+        $this->app->singleton('payment.router', function ($app) {
+            $router = new PaymentRouter();
+
+            $processors = config('payment-router.processors');
+            foreach ($processors as $name => $config) {
+                $class = $config['class'];
+                $router->addProcessor($name, new $class($config));
+            }
+
+            return $router;
+        });
     }
 
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__ . '/config/payment-router.php' => config_path('payment-router.php'),
+        ]);
     }
 }

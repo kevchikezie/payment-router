5af51305558018280509df081f57147bc9976e08
Kelvin Chikezie <kevchike@gmail.com>
2024-07-26 16:21:08 +0100

    Initial commit

---
+++ payment-router/src/PaymentRouterServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kevchikezie\PaymentRouter;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentRoutingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
